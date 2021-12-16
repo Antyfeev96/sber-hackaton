4108a9afec94c706cd6ee261d6c2c7581a0ea303
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-12-16 17:38:59 +0300

    Test deploy

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -8,10 +8,11 @@ const style = {
     height: '50vh'
 };
 
-function MyMap(props) {
+function MyMap() {
     return (
         <YMaps>
             <Wrapper>
+                <div>123</div>
                 <Map
                     style={style}
                     defaultState={{center: [55.75, 37.57], zoom: 9}}
